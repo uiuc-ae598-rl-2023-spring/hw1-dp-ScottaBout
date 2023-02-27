d518af55e45e02af4068efde73790318776d1178
ScottaBout <scott@boutfamily.com>
2023-02-27 13:53:11 -0600

    added TD(0) and pendulum algorithms

--- train_pendulum.py
+++ train_pendulum.py
@@ -1,151 +1,6 @@
 import numpy as np
-import gridworld
+import discrete_pendulum
 import matplotlib.pyplot as plt
-
-
-class PolicyIteration:
-    """
-    Policy Iteration
-    v(s) = E[R_{t+1} + GAMMA * V(S_{t+1} | S_{t} = S]
-    v(s) = r(s) + gamma * SUM p(s' | s,a = pi(s)) v(s')
-    where pi(s) = argmax_{a} [sum_{s'} p(s' | s,a) v(s')
-    """
-
-    def __init__(self, env, gamma):
-        self.env = env
-        self.gamma = gamma
-        self.values = np.zeros(self.env.num_states)
-        self.old_values = np.ones(self.env.num_states)
-        self.actions = np.zeros(self.env.num_states)
-        self.old_actions = np.ones(self.env.num_states)
-        self.values_list_plot = []
-
-    def evaluate(self, total_timesteps=int(1e5)):
-        for k in range(total_timesteps):
-            for s in range(self.env.num_states):
-                self.env.s = s
-                (s1, r1, _) = self.env.step(self.actions[s])
-                p = self.env.p(s1, s, self.actions[s])
-                re = r1 + self.gamma * self.values[s1]
-                self.values[s] = np.sum(p * re)
-
-            self.values_list_plot.append(np.mean(self.values))
-            if np.allclose(self.old_values, self.values, rtol=0, atol=1e-20):
-                break
-            else:
-                self.old_values = self.values.copy()
-
-        return self.values
-
-    def improve(self):
-        v_list = np.zeros(self.env.num_actions)
-        actions = np.zeros(self.env.num_states)
-        for s in range(self.env.num_states):
-            for a in range(self.env.num_actions):
-                self.env.s = s
-                (s1, r1, _) = self.env.step(a)
-                p = self.env.p(s1, s, a)
-                v_list[a] = r1 + self.gamma * np.sum(p * self.values[s1])
-            actions[s] = np.argmax(v_list)
-
-        self.actions = actions
-
-    def learn_actions(self):
-        for k in range(100):
-            self.evaluate()
-            self.improve()
-
-        plt.plot(self.values_list_plot)
-        plt.show()
-
-        return self.actions
-
-    def play(self):
-        actions = self.learn_actions().astype(int)
-        s = self.env.reset()
-        done = False
-        step = 0
-        while not done:
-            self.env.render()
-            a = actions[s]
-            (s, r, done) = self.env.step(a)
-            if done:
-                s = self.env.reset()
-            else:
-                step += 1
-
-    def plot(self):
-        self.learn_actions()
-
-
-class ValueIteration:
-    """
-    Value Iteration
-    v(s) = max_{a} E[R_{t+1} + GAMMA * V(S_{t+1} | S_{t} = S | A_{t} = A]
-    """
-
-    def __init__(self, env, gamma):
-        self.env = env
-        self.gamma = gamma
-        self.values = np.zeros(self.env.num_states)
-        self.old_values = np.ones(self.env.num_states)
-        self.action_space = self.env.num_actions
-        self.values_list_plot = []
-
-    def learn_values(self, total_timesteps=1e5):
-        # iterate over states
-        for k in range(int(total_timesteps)):
-            for s in range(self.env.num_states):
-                v_list = np.zeros(self.env.num_actions)
-                for a in range(self.env.num_actions):
-                    self.env.s = s
-                    s1, r1, _ = self.env.step(a)
-                    p = self.env.p(s1, s, a)
-                    v_list[a] = r1 + self.gamma * np.sum(p * self.values[s1])
-
-                self.values[s] = max(v_list)
-
-            self.values_list_plot.append(np.mean(self.values))
-
-            if np.allclose(self.old_values, self.values, rtol=0, atol=1e-20):
-                plt.plot(self.values_list_plot)
-                plt.show()
-                break
-            else:
-                self.old_values = self.values.copy()
-
-        return self.values
-
-    def get_actions(self, values):
-        v_list = np.zeros(self.env.num_actions)
-        actions = np.zeros(self.env.num_states)
-        for s in range(self.env.num_states):
-            for a in range(self.env.num_actions):
-                self.env.s = s
-                (s1, r1, _) = self.env.step(a)
-                p = self.env.p(s1, s, a)
-                v_list[a] = r1 + self.gamma * np.sum(p * values[s1])
-            actions[s] = np.argmax(v_list)
-
-        return actions.astype(int)
-
-    def play(self):
-        values = self.learn_values()
-        actions = self.get_actions(values)
-        s = self.env.reset()
-        done = False
-        step = 0
-        while not done:
-            self.env.render()
-            a = actions[s]
-            (s, r, done) = self.env.step(a)
-            if done:
-                s = self.env.reset()
-            else:
-                step += 1
-
-    def plot(self):
-        self.learn_values()
 
 
 class SARSA:
@@ -189,7 +44,7 @@ class SARSA:
 
         return self.rew_plot_list
 
-    def learn(self, epsilon_threshold, alpha, total_timesteps=int(1e3)):
+    def learn(self, epsilon_threshold, alpha, total_timesteps=int(1e2)):
         plot_list = []
         for episode in range(total_timesteps):
             list1 = self.rollout(epsilon_threshold=epsilon_threshold, alpha=alpha)
@@ -215,10 +70,12 @@ class SARSA:
         list1 = self.learn(epsilon_threshold=0.1, alpha=1e-5)
         plt.plot(list1)
         self.reset_tables()
+        print(f'learned 1')
 
         list2 = self.learn(epsilon_threshold=0.5, alpha=1e-5)
         plt.plot(list2)
         self.reset_tables()
+        print(f'learned 2')
 
         list3 = self.learn(epsilon_threshold=0.01, alpha=1e-5)
         plt.plot(list3)
@@ -232,10 +89,12 @@ class SARSA:
         list4 = self.learn(epsilon_threshold=0.1, alpha=1e-5)
         plt.plot(list4)
         self.reset_tables()
+        print(f'learned 4')
 
         list5 = self.learn(epsilon_threshold=0.1, alpha=1e-2)
         plt.plot(list5)
         self.reset_tables()
+        print(f'learned 5')
 
         list6 = self.learn(epsilon_threshold=0.1, alpha=1e-7)
         plt.plot(list6)
@@ -295,7 +154,7 @@ class QLearning:
 
         return self.rew_plot_list
 
-    def learn(self, epsilon_threshold, alpha, total_timesteps=int(1e3)):
+    def learn(self, epsilon_threshold, alpha, total_timesteps=int(1e2)):
         plot_list = []
         for episode in range(total_timesteps):
             list1 = self.rollout(epsilon_threshold=epsilon_threshold, alpha=alpha)
@@ -321,10 +180,12 @@ class QLearning:
         list1 = self.learn(epsilon_threshold=0.1, alpha=1e-5)
         plt.plot(list1)
         self.reset_q_table()
+        print(f'learned 1')
 
         list2 = self.learn(epsilon_threshold=0.5, alpha=1e-5)
         plt.plot(list2)
         self.reset_q_table()
+        print(f'learned 2')
 
         list3 = self.learn(epsilon_threshold=0.01, alpha=1e-5)
         plt.plot(list3)
@@ -338,10 +199,12 @@ class QLearning:
         list4 = self.learn(epsilon_threshold=0.1, alpha=1e-5)
         plt.plot(list4)
         self.reset_q_table()
+        print(f'learned 4')
 
         list5 = self.learn(epsilon_threshold=0.1, alpha=1e-2)
         plt.plot(list5)
         self.reset_q_table()
+        print(f'learned 5')
 
         list6 = self.learn(epsilon_threshold=0.1, alpha=1e-7)
         plt.plot(list6)
@@ -407,32 +270,17 @@ class TDzero:
 
 
 if __name__ == '__main__':
-    envs = gridworld.GridWorld()
+    envs = discrete_pendulum.Pendulum(n_theta=15, n_thetadot=21)
     gamma = 0.95
-    """
-    mode = 0 activates ValueIteration
-    mode = 1 activates PolicyIteration
-    mode = 2 activates SARSA
-    mode = 3 activates Q-Learning
-    mode = 4 activates TD(0)
-        submode = 0 activates SARSA
-        submode = 1 activates Q-Learning
-    """
-    mode = 4
-    submode = 1
+    mode = 0
+    submode = 0
     if mode == 0:
-        policy = ValueIteration(envs, gamma)
-        policy.plot()
-    elif mode == 1:
-        policy = PolicyIteration(envs, gamma)
-        policy.plot()
-    elif mode == 2:
         policy = SARSA(envs, gamma)
         policy.plot()
-    elif mode == 3:
+    elif mode == 1:
         policy = QLearning(envs, gamma)
         policy.plot()
-    elif mode == 4:
+    elif mode == 2:
         if submode == 0:
             policy = SARSA(envs, gamma)
             _ = policy.learn(epsilon_threshold=0.1, alpha=1e-5)
